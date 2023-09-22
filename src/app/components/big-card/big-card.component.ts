b731dfdc3a7d9cde7a884d1a0297f6682cd6141c
thurler142 <thurler9@gmail.com>
2023-09-22 16:27:01 -0300

    components dinamycs propertys

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css','./big-card.responsive.component.css']
 })
 export class BigCardComponent {
-
+  @Input()
+  photoCover:string = ''
+  @Input()
+  cardTitle:string = ''
+  @Input()
+  cardDescription:string = ''
+  constructor(){}
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css','./small-card.responsive.component.css']
 })
 export class SmallCardComponent {
+  @Input()
+  photoCover:string = ''
+  @Input()
+  cardTitle:string = ''
+
+
+
+  constructor(){}
 
 }
